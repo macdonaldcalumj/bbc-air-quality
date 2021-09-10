7e67ae1703207c36167d0658fe849188d2ce959f
Calum Macdonald <c@lum.sh>
2021-09-10 12:12:23 +0100

    added i18n plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,30 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "BBC Air Quality",
+  plugins: [
+    {
+      use: "gridsome-plugin-i18n",
+      options: {
+        locales: [
+          // locales list
+          "en-gb",
+          "hi-in",
+        ],
+        pathAliases: {
+          // path segment alias for each locales
+          "en-gb": "en",
+          "hi-in": "hi",
+        },
+        fallbackLocale: "en-gb", // fallback language
+        defaultLocale: "en-gb", // default language
+        enablePathRewrite: true, // rewrite path with locale prefix, default: true
+        rewriteDefaultLanguage: true, // rewrite default locale, default: true
+        messages: {
+          "en-gb": require("./src/locales/en-gb.json"),
+          "hi-in": require("./src/locales/hi-in.json"),
+        },
+      },
+    },
+  ],
+};

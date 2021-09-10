50fe6f9c0facbe7088450dedacdf41d69dd68ba3
Calum Macdonald <c@lum.sh>
2021-09-10 14:50:45 +0100

    added LocaleChanger component

---
+++ src/components/LocaleChanger.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{ $t("message.changeLocale") }}</p>
+    <b-button @click="changeLocale('en-gb')" :type="englishType">English</b-button>
+    <b-button @click="changeLocale('hi-in')" :type="hindiType">हिंदी</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    englishType: function() {
+      return(this.$context.locale === 'en-gb' ? 'is-primary': '')
+    },
+    hindiType: function() {
+      return(this.$context.locale === 'hi-in' ? 'is-primary': '')
+    },
+  },
+  methods: {
+    changeLocale(locale) {
+      // Prevent reloading existing page
+      if (this.$context.locale === locale) {
+        return
+      }
+
+      this.$router.push({
+        path: this.$tp(this.$route.path, locale, true)
+      })
+    }
+  }
+}
+</script>
